ae4a764932aa5533b825c47599be645ed5aeae88
Shreya Sanap <sanapshreya2003@gmail.com>
2024-04-11 21:36:13 +0530

    minor changes

--- client/src/components/Nav/Search-Bar/Form.js
+++ client/src/components/Nav/Search-Bar/Form.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import './Form.css'
 import { useContext } from 'react';
 import { SearchContext } from '../../../Context/SearchContext';
+//
 
 const Form = () => {
     const [searchInput, setSearchInput] = useState('')
